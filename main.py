a8816d74fb7389548f33cfdc52b48b71cf073d05
Matthe2209 <matthiassedlmeier123@gmail.com>
2025-10-07 18:40:28 +0200

    Removed unnecessary comments

--- main.py
+++ main.py
@@ -5,16 +5,12 @@ from modules.db_handler import import_to_postgres, load_config
 def main():
     cfg = load_config()
 
-    # 1. PDF → DataFrame
     df = extract_table_from_pdf("test_data_import.pdf")
 
-    # 2. DataFrame → CSV
     save_to_csv(df, "data.csv")
 
-    # 3. CSV wieder einlesen (optional, zum Testen)
     df_csv = read_from_csv("data.csv")
 
-    # 4. CSV-Daten → PostgreSQL
     import_to_postgres(df_csv, cfg)
 
 if __name__ == "__main__":
